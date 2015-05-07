f972be68438de597f9603f7b8b05fbe7a23d03a7
László Monda <laci@monda.hu>
2015-05-07 19:16:29 +0200

    Handle errors gracefully.

--- bin/kicad-netlist-to-json.js
+++ bin/kicad-netlist-to-json.js
@@ -1,7 +1,23 @@
 #!/usr/bin/env node
+var path = require('path');
 var fs = require('fs');
 var kicadNetlistToJson = require('../index.js');
 
-var kicadNetlist = fs.readFileSync(process.argv[2], {encoding:'utf8'});
+var programName = path.basename(process.argv[1]);
+var kicadNetlistFilename = process.argv[2] || '';
+
+if (kicadNetlistFilename.length === 0) {
+    console.error('Usage: %s kicad-netlist-filename', programName);
+    return;
+}
+
+try {
+    fs.statSync(kicadNetlistFilename);
+} catch(error) {
+    console.error('File does not exists: "%s"', kicadNetlistFilename);
+    return;
+}
+
+var kicadNetlist = fs.readFileSync(kicadNetlistFilename, {encoding:'utf8'});
 var kicadJson = kicadNetlistToJson(kicadNetlist);
 console.log(JSON.stringify(kicadJson, null, 4));
