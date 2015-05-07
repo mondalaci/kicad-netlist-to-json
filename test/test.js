b52920ce47b250de2698fef084e22754ccc63348
László Monda <laci@monda.hu>
2015-05-07 22:02:28 +0200

    Restore healthy build status.

--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@ var kicadNetlistToJson = require('../index.js');
 var kicadNetlist = fs.readFileSync('test/uhk-left-main.net', {encoding:'utf8'});
 var generatedKicadJson = JSON.stringify(kicadNetlistToJson(kicadNetlist), null, 4);
 var savedKicadJson = fs.readFileSync('test/uhk-left-main.json', {encoding:'utf8'});
-var statusCode = generatedKicadJson.trim() === savedKicadJson.trim() ? 1 : 0;
+var statusCode = generatedKicadJson.trim() === savedKicadJson.trim() ? 0 : 1;
 
 console.log(statusCode === 0 ? 'All good.' : 'Test failed!');
 process.exit(statusCode);
