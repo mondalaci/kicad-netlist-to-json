16fede6fbee22fbe86b73a682cb583df1cf3273d
László Monda <laci@monda.hu>
2015-05-07 21:54:24 +0200

    Exit with a status code. Intentionally return an invalid error code to test Travis.

--- test/test.js
+++ test/test.js
@@ -6,6 +6,7 @@ var kicadNetlistToJson = require('../index.js');
 var kicadNetlist = fs.readFileSync('test/uhk-left-main.net', {encoding:'utf8'});
 var generatedKicadJson = JSON.stringify(kicadNetlistToJson(kicadNetlist), null, 4);
 var savedKicadJson = fs.readFileSync('test/uhk-left-main.json', {encoding:'utf8'});
-var testResult = generatedKicadJson.trim() === savedKicadJson.trim() ? 'OK' : 'FAIL';
+var statusCode = generatedKicadJson.trim() === savedKicadJson.trim() ? 0 : 1;
 
-console.log(testResult);
+console.log(statusCode === 0 ? 'All good.' : 'Test failed!');
+process.exit(statusCode);
