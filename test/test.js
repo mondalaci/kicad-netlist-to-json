9a381bb4b2793e481fce7c33e6dee5bde1a1451d
László Monda <laci@monda.hu>
2015-05-08 07:28:43 +0200

    Use relative directory.

--- test/test.js
+++ test/test.js
@@ -3,9 +3,9 @@
 var fs = require('fs');
 var kicadNetlistToJson = require('../index.js');
 
-var kicadNetlist = fs.readFileSync('test/uhk-left-main.net', {encoding:'utf8'});
+var kicadNetlist = fs.readFileSync(__dirname + '/uhk-left-main.net', {encoding:'utf8'});
 var generatedKicadJson = JSON.stringify(kicadNetlistToJson(kicadNetlist), null, 4);
-var savedKicadJson = fs.readFileSync('test/uhk-left-main.json', {encoding:'utf8'});
+var savedKicadJson = fs.readFileSync(__dirname + '/uhk-left-main.json', {encoding:'utf8'});
 var statusCode = generatedKicadJson.trim() === savedKicadJson.trim() ? 0 : 1;
 
 console.log(statusCode === 0 ? 'All good.' : 'Test failed!');
